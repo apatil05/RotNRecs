f909238a5797e13d83a12c8a87611ab20833f762
Tyler Sheehan <tjsheeha@uvm.edu>
2025-12-28 16:50:22 -0500

    initialized fastAPi, fixed database.

---
+++ backend/backend.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+# Create FastAPI app instance
+app = FastAPI()
+
+# Define a simple GET endpoint
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+#adds user info to the DB.
+@app.post("../frontend/register.html")
+def creat_acct():
+    #need to add post code here:
+
+    return {"message": "successfully created user!"}
